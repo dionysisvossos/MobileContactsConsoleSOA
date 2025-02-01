a0e6f7cc6ecfd11f4663a754440ce2e49a11b825
Dionysis.Vossos <dionysis.vossos@windowslive.com>
2025-02-01 17:38:01 +0200

    updates

--- src/gr/aueb/cf/mobilecontacts/controller/MobileContactController.java
+++ src/gr/aueb/cf/mobilecontacts/controller/MobileContactController.java
@@ -1,15 +1,20 @@
 package gr.aueb.cf.mobilecontacts.controller;
 
+import gr.aueb.cf.mobilecontacts.core.serializer.Serializer;
 import gr.aueb.cf.mobilecontacts.dao.IMobileContactDAO;
 import gr.aueb.cf.mobilecontacts.dao.MobileContactDAOImpl;
 import gr.aueb.cf.mobilecontacts.dto.MobileContactInsertDTO;
 import gr.aueb.cf.mobilecontacts.dto.MobileContactReadOnlyDTO;
 import gr.aueb.cf.mobilecontacts.dto.MobileContactUpdateDTO;
+import gr.aueb.cf.mobilecontacts.exceptions.ContactNotFoundException;
 import gr.aueb.cf.mobilecontacts.exceptions.PhoneNumberAlreadyExistsException;
+import gr.aueb.cf.mobilecontacts.mapper.Mapper;
 import gr.aueb.cf.mobilecontacts.model.MobileContact;
 import gr.aueb.cf.mobilecontacts.service.IMobileContactService;
 import gr.aueb.cf.mobilecontacts.service.MobileContactServiceImpl;
 import gr.aueb.cf.mobilecontacts.validation.ValidationUtil;
+
+import java.io.StringReader;
 
 public class MobileContactController {
 
@@ -27,25 +32,53 @@ public class MobileContactController {
            }
 
            // If validation is OK, insert contact
-            mobileContact = service.insertMobileContact(insertDTO);
-           readOnlyDTO = mapMobileContactToDTO(mobileContact);
-           return "OK\n" + serializeDTO(readOnlyDTO);
+           mobileContact = service.insertMobileContact(insertDTO);
+           readOnlyDTO = Mapper.mapMobileContactToDTO(mobileContact);
+           return "OK\n" + Serializer.serializeDTO(readOnlyDTO);
 
         } catch (PhoneNumberAlreadyExistsException e) {
             return "Error\n" + e.getMessage() + "\n";
         }
     }
 
-    private MobileContactReadOnlyDTO mapMobileContactToDTO(MobileContact mobileContact) {
-        return new MobileContactReadOnlyDTO(mobileContact.getId(),
-                                            mobileContact.getFirstname(),
-                                            mobileContact.getLastname(),
-                                            mobileContact.getPhoneNumber());
+    public String updateContact(MobileContactUpdateDTO updateDTO) {
+        MobileContact mobileContact;
+        MobileContactReadOnlyDTO readOnlyDTO;
+        try {
+            // Validate input data
+            String errorVector = ValidationUtil.validateDTO(updateDTO);
+            if (!errorVector.isEmpty()) {
+                return "Error.\n" + "Validation error\n" + errorVector;
+            }
+
+            // If validation is OK, insert contact
+            mobileContact = service.updateMobileContact(updateDTO);
+            readOnlyDTO = Mapper.mapMobileContactToDTO(mobileContact);
+            return "OK\n" + Serializer.serializeDTO(readOnlyDTO);
+        } catch (PhoneNumberAlreadyExistsException e) {
+            return "Error.\n" + e.getMessage() + "\n";
+        } catch (ContactNotFoundException e) {
+            return "Error.\n" + e.getMessage() + "\n";
+        }
+    }
+    public String deleteContactById(Long id) {
+        try {
+            service.deleteContactById(id);
+            return "OK\n Η επαφή διεγράφη";
+        } catch (ContactNotFoundException e) {
+            return "Error\n Λάθος κατά τη διαγραφή. Η επαφή δεν βρέθηκε";
+        }
     }
 
-    private String serializeDTO(MobileContactReadOnlyDTO readOnlyDTO) {
-        return "ID: " + readOnlyDTO.getId() + ", Όνομα:" + readOnlyDTO.getFirstname()
-                + ", Επώνυμο: " + readOnlyDTO.getLastname() + ", Τηλ. Αριθμός: " + readOnlyDTO.getPhoneNumber();
+    public String getContactById(Long id) {
+        MobileContact mobileContact;
+        MobileContactReadOnlyDTO readOnlyDTO;
+        try {
+            mobileContact = service.getContactById(id);
+            readOnlyDTO = Mapper.mapMobileContactToDTO(mobileContact);
+            return "OK\n" + Serializer.serializeDTO(readOnlyDTO);
+        } catch(ContactNotFoundException e) {
+            return "Error \n Η επαφή δεν βρέθηκε";
+        }
     }
-
 }
--- src/gr/aueb/cf/mobilecontacts/mapper/Mapper.java
+++ src/gr/aueb/cf/mobilecontacts/mapper/Mapper.java
@@ -1,6 +1,7 @@
 package gr.aueb.cf.mobilecontacts.mapper;
 
 import gr.aueb.cf.mobilecontacts.dto.MobileContactInsertDTO;
+import gr.aueb.cf.mobilecontacts.dto.MobileContactReadOnlyDTO;
 import gr.aueb.cf.mobilecontacts.dto.MobileContactUpdateDTO;
 import gr.aueb.cf.mobilecontacts.model.MobileContact;
 
@@ -20,6 +21,13 @@ public class Mapper {
 
     public static MobileContact mapUpdateDTOToContact(MobileContactUpdateDTO dto) {
         return new MobileContact(dto.getId(), dto.getFirstname(), dto.getLastname(), dto.getPhoneNumber());
+    }
+
+    public static MobileContactReadOnlyDTO mapMobileContactToDTO(MobileContact mobileContact) {
+        return new MobileContactReadOnlyDTO(mobileContact.getId(),
+                mobileContact.getFirstname(),
+                mobileContact.getLastname(),
+                mobileContact.getPhoneNumber());
     }
 }
 
