c03c6517a9135aab7759422d15c26436a76c1191
Dionysis.Vossos <dionysis.vossos@windowslive.com>
2025-02-01 16:46:49 +0200

    mapper utility class created

--- src/gr/aueb/cf/mobilecontacts/service/IMobileContactService.java
+++ src/gr/aueb/cf/mobilecontacts/service/IMobileContactService.java
@@ -12,14 +12,19 @@ public interface IMobileContactService {
 
     MobileContact insertMobileContact(MobileContactInsertDTO dto)
             throws PhoneNumberAlreadyExistsException;
+
     MobileContact updateMobileContact(MobileContactUpdateDTO dto)
             throws PhoneNumberAlreadyExistsException, ContactNotFoundException;
+
     void deleteContactById(Long id) throws ContactNotFoundException;
+
     MobileContact getContactById(Long id) throws ContactNotFoundException;
+
     List<MobileContact>getAllContacts();
 
     MobileContact getContactByPhoneNumber(String phoneNumber)
             throws  ContactNotFoundException;
+
     void deleteByPhoneNumber(String phoneNumber)
             throws ContactNotFoundException;
 }
